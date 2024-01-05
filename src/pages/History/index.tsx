03fe45527a6563e5351edc167298a4a427972a61
Matheus Momiy <matheus.tadao@gmail.com>
2024-01-05 13:03:08 +0900

    withdraw locale option from history

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from 'react'
 import { formatDistanceToNow } from 'date-fns'
-import ptBR from 'date-fns/locale/pt-BR'
 import { HistoryContainer, HistoryList, Status } from './styles'
 import { CyclesContext } from '../../contexts/CyclesContext'
 
@@ -30,7 +29,6 @@ export function History() {
                   <td>
                     {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true,
-                      locale: ptBR,
                     })}
                   </td>
                   <td>
